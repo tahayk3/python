7f4da2531acf2f9b66eab957308761b0b8ce47c6
tahayk3 <tahayk3@gmail.com>
2024-11-02 10:39:24 -0600

    variables con python

---
+++ variables.py
@@ -0,0 +1,24 @@
+##string##
+name = "Cristian"
+#ver el tipo de variable:  print(type(name))
+print("Hello",name)
+
+##int##
+age = 90
+#incremento age += 1  o age = age +1
+#se puede convertir a string con str str(age)
+print(age)
+
+##float##
+height = 169.5
+print(height)
+
+##bool##
+tired = False
+print("Are you tired?",  tired)
+
+### asignacion en una sola linea
+name2, age2, tired2 = "Cat", 1, False
+print(name2, age2, tired2) 
+
+ 
