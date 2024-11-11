b3c7e8fa0553dbbb4f673c5b47b4cf8b66314f35
tahayk3 <tahayk3@gmail.com>
2024-11-11 10:05:22 -0600

    casting en python

---
+++ typeCasting.py
@@ -0,0 +1,12 @@
+#Es el proceso de convertir un tipo de variable a otro: str(), int(), bool()
+
+name = "Cristian Tahay"
+age = 25
+money= 3.2
+is_student = True
+
+#casting de float a int
+money = int(money)
+print(type(money))
+
+
