c30daf8f89e15cfe9c401a609f9d946241047e42
tahayk3 <tahayk3@gmail.com>
2024-11-11 12:41:35 -0600

    metodos para operaciones matematicas mas utilizados built-in

---
+++ metodosMatematicos.py
@@ -0,0 +1,20 @@
+
+#metodos para operaciones matematicas  módulo built-in, (o módulo estándar). Esto significa que no necesitas importar 
+# ninguna biblioteca adicional para utilizarla.
+
+x = 3.14
+y = -2
+z = 8
+
+#redondear numero
+result = round(x)
+#obtene valor absoluto
+result = abs(y)
+#obtener numeero elevado a otro numero
+result = pow(5,2,3)
+#obtener el numero mayor de una lista de numeros
+result = max(x,y,z)
+#obtener el numero menor de una lista de numeros 
+result = min(x,y,z)
+
+print(result)
