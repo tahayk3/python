719eca2ee170b8bb072d405c9fdf133f0a9e603b
tahayk3 <tahayk3@gmail.com>
2024-11-11 12:10:35 -0600

    input con python

---
+++ input.py
@@ -0,0 +1,13 @@
+#input() = Una función que solicita al usuario que ingrese datos devuelve los datos ingresados ​​como una cadena(string).
+
+name = input("¿Cual es tu nombre?")
+age = input("¿Cual es tu edad?")
+
+age = int(age) +1
+print("Tu nombre es: ", name)
+print("Tu edad es: ", age)
+
+#input con float
+money = float(input("ingresa una cantidad"))
+print("La cantidad ingresada es: ", money)
+
