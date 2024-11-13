f366afa98e7bc284661386869ba8d8631c4441e0
tahayk3 <tahayk3@gmail.com>
2024-11-13 10:39:53 -0600

    manejo de cadenas en python

---
+++ metodosString.py
@@ -0,0 +1,32 @@
+name = input("Ingrese su nombre completo: ")
+
+#obtener cantidad de caracteres
+#result = len(name)
+
+#obtener cuantos caracteres hay hasta la primera coincidencia
+#result = name.find(" ")
+
+#Es lo mismo que find pero del lado derecha
+#result = name.rfind("t")
+
+#Mayuscula en el primer caracter
+#result = name.capitalize()
+
+#todo mayusculas
+#result = name.upper()
+
+#todo minisculas
+#result = name.lower()
+
+#obtener si se tiene un digito, entrega True o False
+#result = name.isdigit()
+
+#obtner si el string contiene solamente caracteres del abecedario
+#result = name.isalpha()
+
+#contar un caracter en especifico del string
+#result = name.count("c")
+
+#metodo que se utiliza para remplazar un caracter
+result = name.replace("c", "h")
+print(result)
