7e54f2d3526173d8eed8463a3875a9011dd332aa
tahayk3 <tahayk3@gmail.com>
2024-11-15 16:18:51 -0600

    manejo de index strings con python

--- indexing.py
+++ indexing.py
@@ -1,2 +1,31 @@
 #indexing = acceso a elementos de una secuencia usando []
 #se tiene [inicio, final, paso en el que avanza]
+
+credit_number = "hola-no-entiendo"
+
+#acceder a un dato en especifico de izquierda a derecha
+#print(credit_number[0])
+
+#acceder a un rango de inicio a fin
+#print(credit_number[2:5])
+
+#acceder de manera normal a un solo valor pero de derecha a izquierda
+#print(credit_number[-3])
+
+#acceder desde un una posicion inicial(lado izquierdo) y mostrar todo lo que esta hacia la derecha
+#print(credit_number[3:])
+
+#mostrar datos desde el inicio hasta el final que indique :NUMERO
+#print(credit_number[:3])
+
+#avanzar de izquierda a derecha de dos en dos
+#print(credit_number[::2])
+
+#avanzar de derecha a izquierda de dos en dos
+#print(credit_number[::-2])
+
+
+#invertir cadena
+#print(credit_number[::-1])
+
+
